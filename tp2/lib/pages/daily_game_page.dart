94b7d9a9b82b24106da2806670c2126668f23014
Aoxcis <gregoirepaul@laposte.net>
2025-02-26 08:58:08 +0100

    feat: add navigation and new pages for game, history, settings, and info

---
+++ tp2/lib/pages/daily_game_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DailyGamePage extends StatefulWidget {
+  const DailyGamePage({super.key});
+
+  @override
+  State<DailyGamePage> createState() => _DailyGamePageState();
+}
+
+class _DailyGamePageState extends State<DailyGamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ tp2/lib/pages/info_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class InfoPage extends StatefulWidget {
+  const InfoPage({super.key});
+
+  @override
+  State<InfoPage> createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Info'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 4,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Text(
+                  'À propos',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Jeu de taquin',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const Divider(height: 32),
+                Text(
+                  'Développé par',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Grégoire Paul et Pierre Provost',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Contact',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'gregoire.paul@etu.imt-nord-europe.fr\npierre.provost@etu.imt-nord-europe.fr',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Code Source',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'https://github.com/Aoxcis/AMSE-TP2.git',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Copyright',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Que dalle pour l\'instant',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
